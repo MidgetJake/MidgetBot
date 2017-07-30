8f33e9e9e65363dd0ff14219933a44c4221babc8
Jake <midg3tjake@live.co.uk>
2017-07-30 15:28:15 +0100

    Patched a weird bug when certain bots might do a message that doesn't return content

--- commands/modCommands.py
+++ commands/modCommands.py
@@ -12,73 +12,76 @@ import json
 async def checkCommand(message, client):
     if message.author.permissions_in(message.channel).administrator:
         msg = message.content.split()
-        if msg[0] == "!bannedWords" or msg[0] == "!bw":
-            with open('config/serverBannedWords') as jsonLoad:
-                bWords = json.load(jsonLoad)
-            try:
-                bList = bWords[message.server.id][message.channel.id]
-                chanAvail = True
-            except KeyError:
-                chanAvail = False
-                bList = []
+        try:
+            if msg[0] == "!bannedWords" or msg[0] == "!bw":
+                with open('config/serverBannedWords') as jsonLoad:
+                    bWords = json.load(jsonLoad)
+                try:
+                    bList = bWords[message.server.id][message.channel.id]
+                    chanAvail = True
+                except KeyError:
+                    chanAvail = False
+                    bList = []
 
-            if msg[1] == "show":
-                await client.send_message(message.channel, bList)
-            elif msg[1] == "add":
-                addWords = msg[2::]
-                bList = bList + addWords
-                if chanAvail:
-                    bWords[message.server.id][message.channel.id] = bList
+                if msg[1] == "show":
+                    await client.send_message(message.channel, bList)
+                elif msg[1] == "add":
+                    addWords = msg[2::]
+                    bList = bList + addWords
+                    if chanAvail:
+                        bWords[message.server.id][message.channel.id] = bList
+                    else:
+                        try:
+                            bWords[message.server.id][message.channel.id] = bList
+                        except:
+                            bWords[message.server.id] = {}
+                            bWords[message.server.id][message.channel.id] = bList
+                    with open('config/serverBannedWords', 'w') as jsonWrite:
+                        json.dump(bWords, jsonWrite)
+                    await client.send_message(message.channel, "Added new words to the banned list!")
+
+            if msg[0] == "!slow" or msg[0] == "!slowmode":
+                with open('config/serverSettings', 'r') as rf:
+                    sSettings = json.load(rf)
+
+                if msg[1].lower() == 'off':
+                    sSettings = checkJson(sSettings, 'slowMode', message, False)
+                    sSettings = checkJson(sSettings, 'slowTime', message, 0)
+                    await client.send_message(message.channel, 'Slow mode is now disabled')
                 else:
                     try:
-                        bWords[message.server.id][message.channel.id] = bList
+                        slowTime = int(float(msg[1]))
+                        sSettings = checkJson(sSettings, 'slowMode', message, True)
+                        sSettings = checkJson(sSettings, 'slowTime', message, slowTime)
+                        with open('slowModeChannels', 'r') as rf:
+                            slowChan = json.load(rf)
+                        slowChan[message.channel.id] = {}
+                        with open('slowModeChannels', 'w') as wf:
+                            json.dump(slowChan, wf)
+                        await client.send_message(message.channel, 'Slow mode is now enabled with a timer of: ' + msg[1] + ' seconds!')
                     except:
-                        bWords[message.server.id] = {}
-                        bWords[message.server.id][message.channel.id] = bList
-                with open('config/serverBannedWords', 'w') as jsonWrite:
-                    json.dump(bWords, jsonWrite)
-                await client.send_message(message.channel, "Added new words to the banned list!")
+                        pass
+                with open('config/serverSettings', 'w') as wf:
+                    json.dump(sSettings, wf)
 
-        if msg[0] == "!slow" or msg[0] == "!slowmode":
-            with open('config/serverSettings', 'r') as rf:
-                sSettings = json.load(rf)
+            if msg[0] == '!mute':
+                with open('config/mutedUsers', 'r') as rf:
+                    mUsers = json.load(rf)
 
-            if msg[1].lower() == 'off':
-                sSettings = checkJson(sSettings, 'slowMode', message, False)
-                sSettings = checkJson(sSettings, 'slowTime', message, 0)
-                await client.send_message(message.channel, 'Slow mode is now disabled')
-            else:
-                try:
-                    slowTime = int(float(msg[1]))
-                    sSettings = checkJson(sSettings, 'slowMode', message, True)
-                    sSettings = checkJson(sSettings, 'slowTime', message, slowTime)
-                    with open('slowModeChannels', 'r') as rf:
-                        slowChan = json.load(rf)
-                    slowChan[message.channel.id] = {}
-                    with open('slowModeChannels', 'w') as wf:
-                        json.dump(slowChan, wf)
-                    await client.send_message(message.channel, 'Slow mode is now enabled with a timer of: ' + msg[1] + ' seconds!')
-                except:
-                    pass
-            with open('config/serverSettings', 'w') as wf:
-                json.dump(sSettings, wf)
+                for user in message.mentions:
+                    mUsers = checkJson(mUsers, user.id, message, True)
 
-        if msg[0] == '!mute':
-            with open('config/mutedUsers', 'r') as rf:
-                mUsers = json.load(rf)
+                with open('config/mutedUsers', 'w') as wf:
+                    json.dump(mUsers, wf)
 
-            for user in message.mentions:
-                mUsers = checkJson(mUsers, user.id, message, True)
+            if msg[0] == '!unmute':
+                with open('config/mutedUsers', 'r') as rf:
+                    mUsers = json.load(rf)
 
-            with open('config/mutedUsers', 'w') as wf:
-                json.dump(mUsers, wf)
+                for user in message.mentions:
+                    mUsers = checkJson(mUsers, user.id, message, False)
 
-        if msg[0] == '!unmute':
-            with open('config/mutedUsers', 'r') as rf:
-                mUsers = json.load(rf)
-
-            for user in message.mentions:
-                mUsers = checkJson(mUsers, user.id, message, False)
-
-            with open('config/mutedUsers', 'w') as wf:
-                json.dump(mUsers, wf)
+                with open('config/mutedUsers', 'w') as wf:
+                    json.dump(mUsers, wf)
+        except:
+            pass
